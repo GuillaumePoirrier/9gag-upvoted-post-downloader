7330ead7de0032e2b3c3cc128c9f46165b16a119
GuillaumePoirrier <guillaume.poirrier@hei.yncrea.fr>
2021-05-13 08:26:27 +0200

    refactoring data processing

--- window.py
+++ window.py
@@ -1,14 +1,15 @@
 from appJar import gui
+from pandas.core.frame import DataFrame
 import requests
 import shutil
 import time
 import datetime
 import os
 import glob
+import pandas as pd
 
 app = gui()
 gag_prefix = 'https://img-9gag-fun.9cache.com/photo/'
-generated_data_filename='data.txt'
 running=True
 t0=0
 
@@ -34,7 +35,7 @@ def createWidgets():
     app.addMeter('progress')
     app.setMeterFill('progress', 'blue')
     app.addStatusbar(header='', fields=1, side=None)
-    app.addButton('Pause', pause)
+    app.addButton('Pause', pauseMode)
     app.disableButton('Pause')
 
 def submit(button):  
@@ -43,8 +44,7 @@ def submit(button):
         outputPath+='/'
         runningMode()
         t0 = time.process_time()
-        convertFileToIdsList(inputPath)
-        app.thread(readIds, outputPath)
+        app.thread(readIds, outputPath, convertFileToIdsList(inputPath))
     else:
         app.errorBox('Input Data', 'Please input your html file and select the output directory', parent=None)
 
@@ -54,20 +54,12 @@ def runningMode():
     app.disableEntry('outputPath')
     app.disableButton('Submit')
 
-def readIds(outputPath):
+def readIds(outputPath, data):
     global running
-    totalCount = countLines(generated_data_filename)
-    ids = open(generated_data_filename, 'r', encoding="utf8")
-    lines = ids.readlines()
-    count = 0
-    for line in lines:
-        line=line.strip()
-        count += 1
-        if not line:
-            break
-        downloadFiles(line, outputPath, count, totalCount)
+    totalCount = len(data)
+    for idx, val in enumerate(data):
+        downloadFiles(val, outputPath, idx+1, totalCount)
         if running==False:
-            ids.close()
             app.setStatusbar('Paused', field=0)
             app.enableButton('Submit')
             app.setButton('Submit', 'Resume')
@@ -75,20 +67,21 @@ def readIds(outputPath):
             app.setButtonCursor('Pause','arrow')
             running = True
             break
-    ids.close()
 
-def downloadFiles(line, outputPath, count, totalCount):
-    extensions = ['_460sv.mp4','_700bwp.webp',  '_460svvp9.webm',
-                    '_460svav1.mp4']
+def downloadFiles(val, outputPath, count, totalCount):
+    extensions = ['_460sv.mp4',
+                  '_700bwp.webp',
+                  '_460svvp9.webm',
+                  '_460svav1.mp4']
     found=False
+    print(val[1])
     for i in range(0, len(extensions)):
         if found == False:
-            if doesExist(line, outputPath) == False:
-                r = requests.get(
-                    gag_prefix + line + extensions[i], stream=True)
+            if doesExist(val, outputPath) == False:
+                r = requests.get(gag_prefix + val[1] + extensions[i], stream=True)
                 if r.status_code == 200:
                     r.raw.decode_content = True
-                    with open(outputPath+line+extensions[i], 'wb') as f:
+                    with open(outputPath+filename(val)+extensions[i], 'wb') as f:
                         shutil.copyfileobj(r.raw, f)
                     app.setMeter('progress', (count/totalCount)*100)
                     app.setStatusbar('Status : ['+str(count)+'/'+str(totalCount)+'] downloaded. Estimated '+ getEstimatedTime(count, totalCount), field=0)
@@ -100,16 +93,19 @@ def downloadFiles(line, outputPath, count, totalCount):
                 app.setStatusbar('[Already found] Status : ['+str(count)+'/'+str(totalCount)+'] downloaded. Estimated '+ getEstimatedTime(count, totalCount), field=0)
 
 
-def pause(button):
+def pauseMode(button):
     global running
     app.setButtonCursor('Pause','watch')
     running = False
 
-def doesExist(filename, outputPath):
+def filename(val):
+    return val[0]+val[1]
+
+def doesExist(val, outputPath):
     exist = False
     files = glob.glob(outputPath+'/*')
     for file in files:
-        if filename in file:
+        if filename(val) in file:
             exist =True
     return exist
 
@@ -117,18 +113,9 @@ def getEstimatedTime(count, totalCount):
     seconds = round(((time.process_time()-t0+totalCount)/count))*60
     return(str(datetime.timedelta(seconds=seconds)))
 
-def countLines(inputPath):
-    # Counting total amount of ids
-    ids = open(inputPath, 'r')
-    totalCount = 0
-    for line in ids:
-        if line != '\n':
-            totalCount += 1
-    ids.close()
-    return totalCount
 
 def convertFileToIdsList(inputPath):
-    with open(inputPath, encoding="utf8") as infile, open('upvotes.txt', 'w', encoding="utf8") as upvotes:
+    with open(inputPath, encoding="utf8") as infile, open('upvoteList.txt', 'w', encoding="utf8") as upvoteList:
         copy = False
         for line in infile:
             if line.strip() == '<h3>Upvotes</h3>':
@@ -138,25 +125,18 @@ def convertFileToIdsList(inputPath):
                 copy = False
                 continue
             elif copy:
-                upvotes.write(line)
-    upvotes.close()
-    infile.close()
+                upvoteList.write(line)
+    upvoteList.close()
     
-    with open('upvotes.txt', encoding="utf8") as upvotes, open('urls.txt', 'w', encoding="utf8") as urls:
-        for line in upvotes:
-            if 'href' in line:
-                urls.write(line)
-    upvotes.close()
-    urls.close()
+    idDataFrame = pd.read_html('upvoteList.txt')[0].values.tolist()
 
-    with open('urls.txt', encoding="utf8") as urls, open(generated_data_filename, 'w', encoding="utf8") as data:
-        for line in urls:
-            data.write(line[46:53]+'\n')
-    data.close()
-    urls.close()
+    for id in idDataFrame:
+        id[1] = id[1][-7:]
+        id[0] = id[0][:10]
+        print(id[0])
 
-    os.remove('upvotes.txt')
-    os.remove('urls.txt')
+    os.remove('upvoteList.txt')
+    return idDataFrame
 
 
 if __name__ == '__main__':
