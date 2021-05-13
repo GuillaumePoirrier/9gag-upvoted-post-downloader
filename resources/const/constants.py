80dc1a0f2a28838742df71abe4e5143779c72602
GuillaumePoirrier <guillaume.poirrier@hei.yncrea.fr>
2021-05-13 10:19:10 +0200

    code optimization and pipenv configuration

---
+++ resources/const/constants.py
@@ -0,0 +1,87 @@
+NINE_GAG_PREFIX = 'https://img-9gag-fun.9cache.com/photo/'
+
+
+##
+# APP WINDOW LABELS
+##
+
+TITLE_LABEL = '9gag upvotes downloader'
+MAIN_LABEL = 'Welcome to 9gag upvotes downloader !'
+INPUT_FILE_PLACEHOLDER = 'Input data html file'
+OUTPUT_PATH_PLACEHOLDER = 'Output directory'
+
+
+
+##
+# APP WINDOW CONTROLNAMES
+##
+
+TITLE = 'title'
+INPUT_FILE = 'inputPath'
+OUTPUT_PATH = 'outputPath'
+PROGRESS_BAR = 'progress'
+
+##
+# BUTTONS
+##
+
+SUBMIT_BUTTON = 'Submit'
+RESUME_BUTTON = 'Resume'
+PAUSE_BUTTON = 'Pause'
+
+##
+# FILE EXTENSIONS
+##
+
+FILE_EXTENSIONS = ['_460sv.mp4',
+                  '_700bwp.webp',
+                  '_460svvp9.webm',
+                  '_460svav1.mp4']
+                  
+##
+# COLOR PROPERTIES
+##
+
+BLUE = 'blue'
+
+##
+# STATUS BAR
+##
+
+STATUS_PAUSE = 'Paused'
+
+##
+# MOUSE ASPECT
+##
+
+MOUSE_ARROW = 'arrow'
+MOUSE_WATCH = 'watch'
+
+
+##
+# TEMPORARY DATA FILE
+##
+
+TEMP_DATA_FILENAME = 'temp.txt'
+
+
+##
+# 9GAG DATA FILE SECTIONS
+##
+UPVOTES_SECTION = '<h3>Upvotes</h3>'
+DOWNVOTES_SECTION = '<h3>Downvotes</h3>'
+
+##
+# FILE PROCESSING STATUS
+##
+STATUS_LABEL = 'Status'
+FINISHED_STATUS = 'finished'
+ALREADY_FOUD_STATUS = 'already found'
+DOWNLOADED = 'downloaded.'
+ESTIMATED = 'Estimated'
+ELAPSED_TIME = ' elapsed time: '
+
+##
+# ERROR MESSAGES
+##
+WRONG_INPUTS = 'Please input your html file and select the output directory'
